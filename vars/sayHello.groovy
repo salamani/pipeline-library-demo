2e3afc4b1e4106731b45e5ecc3feb2cda7bdc0d6
salamani <salamani@us.ibm.com>
2021-11-05 07:20:28 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,5 +2,11 @@
 
 def call(String name = 'human') {
   echo "Hello, ${name}."
+  properties([
+                        parameters([string(name:'CURRENCY_VERSION1',defaultValue:"test",description:'Currency version'),
+                                   string(name:'NODES1',defaultValue:"test",description:'Nodes'),
+                                   string(name:'PRIVATE_RUN_REASON1',defaultValue:"test",description:'Private run reason'),
+                                   string(name:'PUBLISHED_VERSION1',defaultValue:"test",description:'Published version',readOnly:true) ])
+                ])
 }
 
